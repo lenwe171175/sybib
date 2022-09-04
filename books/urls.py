8f7ea10e5fdfdfead4ce163fe1f2802c7628b639
lenwe171175 <robin.sylvestre@live.fr>
2022-09-04 11:06:09 +0200

    Code simplification

--- books/urls.py
+++ books/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path("add_book_scan", views.add_book_scan, name="add_book_scan"),
-    path("add_book", views.add_book, name="add_book"),
+    # path("add_book_scan", views.add_book_scan, name="add_book_scan"),
+    # path("add_book", views.add_book, name="add_book"),
+    path("add_book_scan", views.AddBook.as_view(template_name="books/add_book_scan.html"), name="add_book_scan"),
+    path("add_book", views.AddBook.as_view(template_name="books/add_book.html"), name="add_book"),
     path("", views.index, name="index"),
 ]
--- books/views.py
+++ books/views.py
@@ -6,35 +6,23 @@ from django.contrib import messages
 
 from .models import Book
 
+from django.views import View
+
 # Create your views here.
 
 def index(request):
     return render(request, "books/index.html") 
 
-def add_book_scan(request):
-    if request.method == "POST":
-        form = BookForm(request.POST)
-        if form.is_valid():
-            form = form.save(commit=False)
-            isbn=form.isbn
-            if Book.objects.filter(isbn=isbn).count() == 1:
-                target_book=Book.objects.get(isbn=isbn)
-                target_book.quantity+=1
-                target_book.save()
-                messages.success(request, "ISBN déjà existant - quantité modifiée")
-            else:
-                form.save()
-                messages.success(request, "Ajout réussi")
-            return redirect(index)
-        else:
-            messages.error(request, "Une erreur est survenue")
-    else:
-        form = BookForm()
-    return render(request, "books/add_book_scan.html", {"form": form})
+class AddBook(View):
+    form_class = BookForm
+    template_name = "books/add_book.html"
 
-def add_book(request):
-    if request.method == "POST":
-        form = BookForm(request.POST)
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form}) 
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
         if form.is_valid():
             form = form.save(commit=False)
             isbn=form.isbn
@@ -49,6 +37,4 @@ def add_book(request):
             return redirect(index)
         else:
             messages.error(request, "Une erreur est survenue")
-    else:
-        form = BookForm()
-    return render(request, "books/add_book.html", {"form": form})
+        return render(request, self.template_name, {'form': form})
